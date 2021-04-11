b2a7d52be75652bd0bd2e658a2a39cd424d7dee6
zich188 <78915934+zich188@users.noreply.github.com>
2021-04-11 11:38:17 +0200

    ComposerAutoload

---
+++ COMPOSERAUTOLOAD/public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+//use App\Wcs\Hello;
+
+$zach = new App\Wcs\Hello();
+echo $zach->talk();
---
+++ COMPOSERAUTOLOAD/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'zach/5.1composer-autoload',
+  ),
+  'versions' => 
+  array (
+    'zach/5.1composer-autoload' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
